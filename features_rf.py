c3768cc40c34687fd1dfec3eb5996828d7930691
juldaani <juho.uusiluomalahti@gmail.com>
2019-05-09 16:19:43 +0300

    developing wrapper classes

--- features_rf.py
+++ features_rf.py
@@ -9,6 +9,7 @@ Created on Mon Apr  1 20:18:43 2019
 import numpy as np
 import pandas as pd
 from holdem_hu_bot.game_data_container import GameDataContainer
+from numba import jit
 
 """
 - effective stack
@@ -21,32 +22,15 @@ from holdem_hu_bot.game_data_container import GameDataContainer
 """
 
 
-# %%
 
-from texas_hu_engine.wrappers import initRandomGames, GameState, executeActions
+from texas_hu_engine.wrappers import initRandomGames, GameState, executeActions, createActionsToExecute
 from texas_hu_engine.engine_numba import getBoardCards, getGameEndState
 from equity_calculator.equity_calculator import computeEquities
 from holdem_hu_bot.common_stuff import suitsOnehotLut, ranksOnehotLut, encodeCardsOnehot
 
 
-#%%    
-#
-#def encodeCardsOnehot(cards, visibleCardsMask, ranksOnehotLut, suitsOnehotLut):
-#    visibleCardsMaskRanks = np.repeat(visibleCardsMask, ranksOnehotLut.shape[1], axis=1)
-#    visibleCardsMaskSuits = np.repeat(visibleCardsMask, suitsOnehotLut.shape[1], axis=1)
-#    cardSuitsOnehot = suitsOnehotLut[cards].reshape((len(cards), cards.shape[1]*suitsOnehotLut.shape[1]))
-#    cardRanksOnehot = ranksOnehotLut[cards].reshape((len(cards), cards.shape[1]*ranksOnehotLut.shape[1]))
-#    cardSuitsOnehot[~visibleCardsMaskSuits.astype(np.bool)] = 0    # Set nonvisible cards to zero
-#    cardRanksOnehot[~visibleCardsMaskRanks.astype(np.bool)] = 0
-#    
-#    return cardSuitsOnehot, cardRanksOnehot
-
-
-
-    
     
 
-# %%
 
 
 class Features(GameDataContainer):
@@ -110,7 +94,7 @@ class Features(GameDataContainer):
                                                                      np.ones(holecards.shape, dtype=np.bool), 
                                                                      ranksOnehotLut, suitsOnehotLut)
 
-        gameValidMask = ~controlVariables[:,1].astype(np.bool)    # Tells if the game is still on
+        gameValidMask = ~controlVariables[:,1].astype(np.bool)    # Tells if the current game is still on
         bettingRound = visibleCardsMask[:,2:].astype(np.int)
 
         # Get equities
@@ -133,65 +117,122 @@ class Features(GameDataContainer):
                 
                 'gameValidMask':gameValidMask}
         
+        
+        
+        
+
+
+from abc import ABC, abstractmethod
+
+
+class Agent(ABC):
+    
+    def __init__(self, playerNumber):
+        self.playerNumber = playerNumber
+    
+    @abstractmethod
+    def getActions(self, gameStates):
+        pass
+    
+        
+@jit(nopython=True, cache=True, fastmath=True, nogil=True)
+def generateRndActions(availableActions):
+    foldProb = 0.1
+    foldProbArr = np.zeros(100, dtype=np.bool_)
+    foldProbArr[:int(foldProb*100)] = True
+    
+    allInRaiseProb = 0.1
+    allInRaiseProbArr = np.zeros(100, dtype=np.bool_)
+    allInRaiseProbArr[:int(allInRaiseProb*100)] = True
+    
+    amounts = np.zeros(len(availableActions), dtype=np.int64)
+    for i in range(len(availableActions)):
+        curActions = availableActions[i]
+        
+        # Fold ?
+        isFold = foldProbArr[np.random.randint(len(foldProbArr))]
+        if(isFold):
+            amounts[i] = -1
+            continue
+        
+        # Call or raise ?
+        callOrRaise = np.random.randint(np.sum(curActions[:2] > -1))  # 0 = call, 1 = raise
+        if(callOrRaise == 0):   # Call
+            amounts[i] = curActions[callOrRaise]
+            continue
+        if(callOrRaise == 1):   # Raise
+            isAllInRaise = allInRaiseProbArr[np.random.randint(len(allInRaiseProbArr))]
+            if(isAllInRaise):
+                amounts[i] = curActions[-1]
+                continue
+            
+            lowRaise, highRaise = curActions[1], curActions[2]
+            amounts[i] = np.random.randint(lowRaise, highRaise+1)
+            
+    return createActionsToExecute(amounts)
+
+
+class RndAgent(Agent):
+    
+    def getActions(self, gameStates):
+        playerNum = self.playerNumber
+        
+        actingPlayerNum = np.argmax(gameStates.players[:,6].reshape((-1,2)),1)
+        playerMask = actingPlayerNum == playerNum
+        gameEndMask = gameStates.controlVariables[:,1] == 1
+        gameFailedMask = gameStates.controlVariables[:,1] == -999
+        mask = playerMask & ~gameEndMask & ~gameFailedMask
+        
+        availableActions = gameStates.availableActions[mask]
+        
+        return generateRndActions(availableActions), mask
+
+
+
+class PlayGames:
+
+    def __init__(self, agents):
+        self.agents = agents
+    
+    def initRandomGames(self, numGames):    
+        self.gameStates = initRandomGames(numGames)
+
+#availableActions = gameStates.availableActions[[1,4,-1,-2]]
+#aaa = generateRndActions(np.tile(availableActions,(1000000,1)))
+
 
 # %%
+
+agents = [RndAgent(0), RndAgent(1)]
+#agent.getActions(gameStates)
+
+
 
 N = 10
 
 gameStates = initRandomGames(N)
-asd = Features(N)
 
 # %%
 
-asd.addData(gameStates)
+actionsAgent0, maskAgent0 = agents[0].getActions(gameStates)
+actionsAgent1, maskAgent1 = agents[1].getActions(gameStates)
 
-rndActions = gameStates.availableActions[np.arange(N),np.random.randint(0,3, size=N)]
-rndActions = np.column_stack((np.ones(N)*-1, rndActions))
-mask = gameStates.availableActions >= 0
+actionsToExecute = np.zeros((len(gameStates.availableActions),2), dtype=np.int64)-999
+actionsToExecute[maskAgent0] = actionsAgent0
+actionsToExecute[maskAgent1] = actionsAgent1
 
+gameStates = executeActions(gameStates, actionsToExecute)
 
-gameStates = executeActions(gameStates, rndActions)
+print(np.sum(gameStates.controlVariables[:,1]==0))
 
-print(np.sum(gameStates.validMask))
+#gameStates.controlVariables
+#np.argmax(gameStates.players[:,6].reshape((-1,2)),1)
 
-
-# %%
-
-#asd.equities[1]['flop']
-#eq = asd.equities
-
-ddd = asd.getFeatures()
-
-ddd['equities'].shape
 
 # %%
 
 
-cardToSuit = np.zeros((52,4), dtype=np.int8)
-cardToRank = np.zeros((52,13), dtype=np.int8)
-ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
-for cardNum in intToCard:
-    card = intToCard[cardNum]
-    print(cardNum,card)
-    
-    cardToSuit[cardNum,0], cardToSuit[cardNum,1] = 'c' in card, 'd' in card
-    cardToSuit[cardNum,2], cardToSuit[cardNum,3] = 'h' in card, 's' in card
-    cardToRank[cardNum] = [rank in card for rank in ranks]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# %%
   
     
     
