2be23aa652a12bea2d1ef2f187e268c1a7027948
juldaani <juho.uusiluomalahti@gmail.com>
2019-05-13 00:23:29 +0300

    reorganize code

--- features_rf.py
+++ features_rf.py
@@ -17,25 +17,40 @@ from holdem_hu_bot.common_stuff import suitsOnehotLut, ranksOnehotLut, encodeCar
 
 class RfFeatures(GameDataContainer):
     
-    def __init__(self, nGames): 
-        GameDataContainer.__init__(self, nGames)
-        self.equities = None
+#    def __init__(self, nGames): 
+#        GameDataContainer.__init__(self, nGames)
+#        self.equities = None
+    
+    def __init__(self, gameStates):
+        equities = {0:{}, 1:{}}
+   
+        boardCards = gameStates.boards[:,-5:]
+        player0HoleCards = gameStates.players[::2,:2]
+        player1HoleCards = gameStates.players[1::2,:2]
+        
+        equities[0]['preflop'], equities[0]['flop'], equities[0]['turn'], \
+            equities[0]['river'] = computeEquities(player0HoleCards, boardCards)
+        equities[1]['preflop'], equities[1]['flop'], equities[1]['turn'], \
+            equities[1]['river'] = computeEquities(player1HoleCards, boardCards)
+        
+        self.equities = equities
+        GameDataContainer.__init__(self, len(boardCards))
         
         
     def addData(self, gameStates, actions):
-        if(self.equities == None):
-            equities = {0:{}, 1:{}}
-       
-            boardCards = gameStates.boards[:,-5:]
-            player0HoleCards = gameStates.players[::2,:2]
-            player1HoleCards = gameStates.players[1::2,:2]
-            
-            equities[0]['preflop'], equities[0]['flop'], equities[0]['turn'], \
-                equities[0]['river'] = computeEquities(player0HoleCards, boardCards)
-            equities[1]['preflop'], equities[1]['flop'], equities[1]['turn'], \
-                equities[1]['river'] = computeEquities(player1HoleCards, boardCards)
-            
-            self.equities = equities
+#        if(self.equities == None):
+#            equities = {0:{}, 1:{}}
+#       
+#            boardCards = gameStates.boards[:,-5:]
+#            player0HoleCards = gameStates.players[::2,:2]
+#            player1HoleCards = gameStates.players[1::2,:2]
+#            
+#            equities[0]['preflop'], equities[0]['flop'], equities[0]['turn'], \
+#                equities[0]['river'] = computeEquities(player0HoleCards, boardCards)
+#            equities[1]['preflop'], equities[1]['flop'], equities[1]['turn'], \
+#                equities[1]['river'] = computeEquities(player1HoleCards, boardCards)
+#            
+#            self.equities = equities
             
         super().addData(gameStates, actions)
     
@@ -52,28 +67,27 @@ class RfFeatures(GameDataContainer):
         
     
     def getFeaturesForAllGameStates(self):
-        indexes, gameNumbers = super().getAllIndexes()
-        
-        return self.computeFeatures(indexes, gameNumbers)
-    
-    
-    def computeFeatures(self, indexes, gameNumbers):
         data, _ = super().getData()
-        
-#        indexes, gameNumbers = rfFeatures.getAllIndexes()   # TODO: remove
-#        data,_ = rfFeatures.getData()   # TODO: remove
+        indexes, gameNumbers = super().getAllIndexes()
         
         boardsData = data['boardsData'][indexes]
         playersData = data['playersData'][indexes]
         availableActions = data['availableActionsData'][indexes]
         controlVariables = data['controlVariablesData'][indexes]
-        executedActions = data['actions'][indexes]
+#        executedActions = data['actions'][indexes]
         
+        features, miscDict = self.computeFeatures(boardsData, playersData, availableActions, 
+                                                  controlVariables, gameNumbers)
+        executedActions = self.getExecutedActionsNormalized(indexes)
+        
+        return features, executedActions, miscDict
+    
+    
+    def getExecutedActionsNormalized(self, indexes):
+        data, _ = super().getData()
+        executedActions = data['actions'][indexes]
+        boardsData = data['boardsData'][indexes]
         smallBlinds = np.row_stack(boardsData[:,1]) # Small blinds amounts are used for normalization
-        actingPlayerIdx = np.argmax(playersData[:,[6,14]], 1)
-        nonActingPlayerIdx = (~actingPlayerIdx.astype(np.bool)).astype(np.int)
-        isSmallBlindPlayer = playersData[:,[4,12]][np.arange(len(actingPlayerIdx)),actingPlayerIdx].astype(
-                np.uint8)
         
         # Normalize executed actions
         mask1 = executedActions == -1
@@ -83,6 +97,16 @@ class RfFeatures(GameDataContainer):
         executedActions[mask1] = -1
         executedActions[mask2] = -999
         executedActions[mask3,0] = 1
+        
+        return executedActions
+    
+    
+    def computeFeatures(self, boardsData, playersData, availableActions, controlVariables, gameNumbers):
+        smallBlinds = np.row_stack(boardsData[:,1]) # Small blinds amounts are used for normalization
+        actingPlayerIdx = np.argmax(playersData[:,[6,14]], 1)
+        nonActingPlayerIdx = (~actingPlayerIdx.astype(np.bool)).astype(np.int)
+        isSmallBlindPlayer = playersData[:,[4,12]][np.arange(len(actingPlayerIdx)),actingPlayerIdx].astype(
+                np.uint8)
         
         # Pots, stacks etc. money stuffs
         pots = boardsData[:,0]
@@ -127,12 +151,12 @@ class RfFeatures(GameDataContainer):
                     'gameFinishedMask':gameFinishedMask, 'gameFailedMask':gameFailedMask, 
                     'actingPlayerIdx':actingPlayerIdx}
         
-        features = np.column_stack((isSmallBlindPlayer, bettingRound, equities, #availableActionsNormalized,
+        features = np.column_stack((isSmallBlindPlayer, bettingRound, #equities, #availableActionsNormalized,
                                     potsNormalized, ownStacksNormalized, opponentStacksNormalized,
                                     boardcardSuitsOnehot, boardcardRanksOnehot, holecardSuitsOnehot, 
                                     holecardRanksOnehot))
 
-        return features, executedActions, miscDict
+        return features, miscDict
         
         
         
